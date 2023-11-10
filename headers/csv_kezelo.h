90b4ee86042b690143cd0efa33fc7dae265572a7
RobiiiHUN <58343667+RobiiHUN@users.noreply.github.com>
2023-11-10 11:24:26 +0100

    11.10

--- headers/csv_kezelo.h
+++ headers/csv_kezelo.h
@@ -1,3 +1,6 @@
+#ifndef CSV_KEZELO_H
+#define CSV_KEZELO_H
+
 #include <stdio.h>
 #include <string.h>
 
@@ -8,29 +11,8 @@ typedef struct
     int felev_tipusa;
 }Hallgatok_alapadatok;
 
-int csv_sorolvaso(FILE *fajlnev, Hallgatok_alapadatok *hallgato_adatok){
-    
-    int beolvasas = fscanf(fajlnev, "%49[^,], %d, %d\n", &hallgato_adatok->nev, &hallgato_adatok->neptun_kod, &hallgato_adatok->felev_tipusa);
-    
-    if ( beolvasas>0)
-    {
-        
-        return 1;
-    } else{
-        return 0;
-    }
-    
+int csv_sorolvaso(FILE *fajlnev, Hallgatok_alapadatok *hallgato_adatok);
+int csv_alapadat_kiir(FILE *fajlnev, char *nev_vez, char *nev_ker,int neptunkod, int felev_tipusa);
+int be_kiiratkozas(FILE *fajlnev, Hallgatok_alapadatok *hallgato, const int uj_ertek );
 
-}
-
-int csv_alapadat_kiir(FILE *fajlnev, char *nev_vez, char *nev_ker,int neptunkod, int felev_tipusa){
-    fprintf(fajlnev, "%s %s, %d, %d\n", nev_vez, nev_ker, neptunkod, felev_tipusa);
-    return 0;   
-}
-
-
-int be_kiiratkozas(FILE *fajlnev, Hallgatok_alapadatok *hallgato, const char *uj_ertek )
-{
-    strncpy(hallgato -> felev_tipusa, uj_ertek, sizeof(hallgato->felev_tipusa)-1);
-    return 0;
-}
+#endif
