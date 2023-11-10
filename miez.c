bbae3776b6cd80a6843ab2dc1419d89b4cd0b438
RobiiiHUN <58343667+RobiiHUN@users.noreply.github.com>
2023-11-10 13:43:57 +0100

    11.10

--- miez.c
+++ miez.c
@@ -3,6 +3,13 @@
 
 
 int main(void){
+    char tomb[20] = {"elso", "masodik"};
+    char test[20] = {"egy", "ketto"};
+
+    for (int i = 0; i < 20; i++)
+    {
+        printf("%c %c", tomb[i], test[i]);
+    }
     
     return 0;
 }
